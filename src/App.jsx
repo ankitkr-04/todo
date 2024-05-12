468583bdf4222d61bdc9491b2365010742ac9717
Ankit Kumar <ak0182274@gmail.com>
2024-05-12 08:58:31 +0530

    todo Input component inti

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import TodoList from './components/TodoList'
+import TodoInput from './components/TodoInput'
 
 function App() {
   return (
-    <>
-     
-    </>
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
   )
 }
 
